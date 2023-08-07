75729f24b3bcdf6202a3ce9eacf7f1b66760337b
AlbertoA01285213 <a01285213@tec.mx>
2023-08-07 07:55:08 -0600

    Create Prueba_motores.ino

---
+++ arduino/Prueba_motores/Prueba_motores.ino
@@ -0,0 +1,98 @@
+#include <MotoresRave.h>
+#include <Ultrasonicos.h>
+#include <Ventilador.h>
+#include <Flama.h>
+#include <Seguidor.h>
+//#include <Servo.h>
+//#include "Wire.h"
+
+MotoresRave motores(12, 13, 11, 7, 8, 6, A0, 4, 5); // Motor derecho, motor izquierdo, motor trasero
+// viéndolo de frente al robot. Cada uno tiene 3 pines: enable, input1 e input2
+
+Ultrasonicos sensoresUltrasonicos(A1, A3, 2, A5); // Sensor ultrasónico izquierdo y derecho. Cada uno tiene
+// 2 pines: trig y echo
+
+//Flama flama(A4, 100); // Sensor de flama. Tiene 1 pin y un valor mínimo de detección de flama
+
+//Servo ventiladorServo; // Objeto servomotor para el ventilador
+//Ventilador ventilador(ventiladorServo, 5); // Servomotor del ventilador con objeto servomotor y pin
+
+short seguidorPin = 3; // Variable que contiene pin digital 2 de seguidor de línea
+
+int angulo = 0; // Ángulo en donde se encuentra la vela prendida con valor dividido por 4 para permitir la recepción de valores execedentes a 255 en bytes
+int x = 0; // Variable con valor de angulo multiplicado por cuatro (verdadero ángulo)
+bool vela = false; // Variable que menciona la detección o no de una vela
+bool fuego = false; // Variable que menciona la detección o no de flama
+int distanciaIz; // Variable que contiene la distancia detectada por el sensor ultrasónico izquierdo de un objeto externo
+int distanciaDe; // Variable que contiene la distancia detectada por el sensor ultrasónico derecho de un objeto externo
+boolean vela_prendida = false; // Variable que menciona si una vela está prendida
+int velocidad = 85;
+String command = "";
+
+void setup() {
+
+  Serial.begin(9600); // Inicio de comunicación serial a 9600 bits/s
+  pinMode(seguidorPin,INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(seguidorPin), detectar, HIGH);
+  
+  motores.iniciar(); // Asignación de pines de entradas 1 y 2 y el enable como salidas
+  
+  sensoresUltrasonicos.iniciar(); // Asignaciones de pines y modos de pines a sensores ultrasónicos
+  // Aquí, los pines TRIG son OUTPUT y los pines ECHO son INPUT
+  
+  
+ // ventilador.calibrar(); // Inicio y calibración de ventilador
+
+}
+
+void loop() {
+    
+    if (Serial.available() > 0) {
+      command = "";
+      command = Serial.readStringUntil('\n');
+      command.trim();
+      
+    }
+
+    angulo = command.toInt();
+    x = angulo * 4;
+
+    if (x > 290 && x < 350) {
+      motores.movimiento(0,velocidad);
+
+      distanciaIz = sensoresUltrasonicos.getDistanciaIz();
+      distanciaDe = sensoresUltrasonicos.getDistanciaDe();
+      if (distanciaIz < 10){     // Moverse a la derecha porque hay pared a la izquierda
+        motores.movimiento(300, velocidad);
+      }
+      
+      if (distanciaDe < 10){     // Moverse a la izquierda porque hay pared a la derecha
+        motores.movimiento(60, velocidad);
+      }
+      
+    }
+ 
+
+    if (x < 290){
+      motores.giro(0, velocidad);
+    }
+
+    if (x > 350){
+      motores.giro(1, velocidad);
+    }    
+}
+
+void detectar(){
+  delay(1000);
+  motores.alto();
+  delay(1000);
+
+  if (digitalRead(A4) == 1){
+    delay(2000);
+  }
+  
+  if (digitalRead(A4) == 0){
+    motores.giro(1, velocidad);
+    delay(1000);
+  }
+}
